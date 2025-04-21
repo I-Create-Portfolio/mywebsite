06de2d175ec711b223b513a9114af41585c4c4a5
I-Create-Portfolio <gordwincreative@gmail.com>
2025-04-21 10:26:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -669,3 +669,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 })(window.jQuery);
+
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       // Add show-menu class to nav menu
+       nav.classList.toggle('show-menu')
+
+       // Add show-icon to show and hide the menu icon
+       toggle.classList.toggle('show-icon')
+   })
+}
+
+showMenu('nav-toggle','nav-menu')
