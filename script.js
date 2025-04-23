d75bf1a46340a7d850ec49b4c9775f836e614aca
I-Create-Portfolio <gordwincreative@gmail.com>
2025-04-23 04:24:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -525,3 +525,26 @@ sr.reveal('.skills-img', {delay: 400} )
 sr.reveal('.work-img', {interval: 200} )
 
 sr.reveal('.contact-input', {interval: 200} )
+
+var timerID;
+window.onload = function() {
+  canvas = document.getElementById('myCanvas');
+  ctx = canvas.getContext('2d');
+
+  var video = document.getElementById('video');
+
+  video.addEventListener('play', function() {
+    video.currentTime = 0;
+    timerID = window.setInterval(function() {
+      ctx.drawImage(video, 0, 0, 600, 460)
+    }, 30);
+  });
+
+  video.addEventListener('pause', function() {
+    stopTimer();
+  });
+
+  video.addEventListener('ended', function() {
+    stopTimer();
+  });
+}
